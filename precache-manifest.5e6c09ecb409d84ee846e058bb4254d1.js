cfab64d798f5cd935ec65b3494e1807250f54109
elgaridhomaulana <elgaridhomaulana@gmail.com>
2020-01-15 09:47:20 +0700

    Updates

---
+++ precache-manifest.5e6c09ecb409d84ee846e058bb4254d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73d87c35e6713513c0d087079ea1e0e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7efc70821facfcca08ad",
+    "url": "/robofriends/static/css/main.95bfcf30.chunk.css"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriends/static/js/2.b0983e84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.b0983e84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7efc70821facfcca08ad",
+    "url": "/robofriends/static/js/main.e2ffa8e8.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  },
+  {
+    "revision": "afeaf6797b6286b72b613ababc563807",
+    "url": "/robofriends/static/media/BebasNeue-Regular.afeaf679.woff"
+  }
+]);
